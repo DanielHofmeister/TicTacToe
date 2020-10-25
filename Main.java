a5222248763a87313509c8034a4f008efcb0f75d
Dan Hofmeister <dhofmeister@wisc.edu>
2020-10-25 00:57:59 -0500

    added one player method

--- Main.java
+++ Main.java
@@ -1,27 +1,34 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class Main {
-    static int numMoves = 0;
+    private static int numMoves = 0;
+    private static Scanner sc = new Scanner(System.in);
+    private static char[][] board;
     public static void main(String[] args) {
-        char[][] board = new char[3][3];
+        board = new char[3][3];
         setup(board);
-        printOutline(board);
-        Scanner sc = new Scanner(System.in);
 
         System.out.println("Do you want to play tic tac toe? (Y/N)");
         String input = sc.nextLine();
-        do {
+
+        if (input.equalsIgnoreCase("N") || (input.equalsIgnoreCase("NO"))) {
+            System.out.println("Thanks for playing!");
+        }
+        else if (input.equalsIgnoreCase("Y") || (input.equalsIgnoreCase("YES"))) {
             System.out.println("Number of Players? (1/2)");
             input = sc.nextLine();
             if (input.equals("1")) {
                 onePlayer();
             }
-            if (input.equals("2")) {
-                twoPlayer(sc, board);
+            else if (input.equals("2")) {
+                twoPlayer();
+            } else {
+                System.out.println("Invalid Input: " + input);
             }
-
-        } while (!input.equalsIgnoreCase("N"));
-
+        } else {
+            System.out.println("Invalid Input: " + input);
+        }
         sc.close();
     }
     private static void setup(char[][] board) {
@@ -31,7 +38,7 @@ class Main {
             }
         }
     }
-    private static void printOutline(char[][] board) {
+    private static void printOutline() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j]);
@@ -40,31 +47,111 @@ class Main {
         }
     }
     private static void onePlayer() {
+        boolean gameOver = false;
+        int playerNum = 1;
+        int[] move = new int[2];
+        while (!gameOver) {
+            if (playerNum == 1) {
+                move = playerInput(playerNum);
+            } else {
+                printOutline();   
+                System.out.println("Computer's Turn");
+                move = MiniMax.bestMove(board);
+            }
+            if (playerNum == 1) {
+                board[move[0]][move[1]] = 'X';
+            } else {
+                board[move[0]][move[1]] = 'O';
+            }
+            numMoves++;
+            if (checkWin()) {
+                System.out.println("Player " + playerNum + " wins!");
+                gameOver = true;
+            } else if (numMoves == 9) {
+                System.out.println("Tie Game");
+                gameOver = true;
+            }
 
-    }
-    private static void twoPlayer(Scanner sc, char[][] board) {
-        while (numMoves < 9) {
-            // Player 1
-            if (numMoves%2 == 0) {
-                System.out.println("Player One's Turn: Enter a coordinate");
-                String input = sc.nextLine();
-                int row = input.charAt(0);
-                int col = input.charAt(1);
-                if (validSquare(board, row, col)) {
-                    board[row][col] = 'x';
-                } else {
-                    System.out.println("Invalid Coordinate");
-                }
-            } 
-            // Player 2
-            else {
-                System.out.println("Player Two's Turn: Enter a coordinate");
-                String input = sc.nextLine();
-                
+            if (playerNum == 1) {
+                playerNum++;
+            } else {
+                playerNum--;
             }
         }
     }
-    private static boolean validSquare(char[][]board, int row, int col) {
+    private static int[] playerInput(int playerNum) {
+        int[] coordinate = new int[] {-1, -1};
+        do {     
+            printOutline();   
+            try {
+                System.out.println("Player " + playerNum + "'s Turn: Enter a row");
+                int row = sc.nextInt();
+                coordinate[0] = row;
+
+                System.out.println("Player " + playerNum + "'s Turn: Enter a column");
+                int col = sc.nextInt();
+                coordinate[1] = col;
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a valid row and column.");
+                sc.nextLine();
+            }
+        } while (!validSquare(coordinate[0], coordinate[1]));
+            return coordinate;
+    }
+    private static void twoPlayer() {
+        boolean gameOver = false;
+        int playerNum = 1;
+        while (!gameOver) {
+            int[] move = playerInput(playerNum);
+            if (playerNum == 1) {
+                board[move[0]][move[1]] = 'X';
+            } else {
+                board[move[0]][move[1]] = 'O';
+            }
+            numMoves++;
+            if (checkWin()) {
+                System.out.println("Player " + playerNum + " wins!");
+                gameOver = true;
+            } else if (numMoves == 9) {
+                System.out.println("Tie Game");
+                gameOver = true;
+            }
+
+            if (playerNum == 1) {
+                playerNum++;
+            } else {
+                playerNum--;
+            }
+        }
+    }
+    private static boolean validSquare(int row, int col){
+            if (row > 2 || col > 2 || row < 0 || col < 0) {
+                return false;
+            }
+            else if (board[row][col] != '-') {
+                return false;
+            }
+            else {
+                return true;
+            }
+    }
+    private static boolean checkWin() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != '-') {
+                return true;
+            } else if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != '-') {
+                return true;
+            }
+        }
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '-') {
+            return true;
+        }
+        else if (board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[2][0] != '-') {
+            return true;
+        } else {
+            return false;
+        }
 
     }
+    
 }
